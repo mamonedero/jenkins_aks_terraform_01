c31758d10f43aa1d5b62d05cb1595617f85300ce
Miguel Angel Monedero <miguel.monedero@eviden.com>
2025-11-21 13:44:21 +0100

    primer commit

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" { default = "rg-aks-demo" }
+variable "location" { default = "eastus" }
+variable "aks_name" { default = "aks-demo" }
+variable "acr_name" { default = "acraksdemo" }
